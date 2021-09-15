66954bcb4f72079c9760a39ac80866f0fe25f6c8
ljinen <38338710+ljinen@users.noreply.github.com>
2021-09-15 21:53:33 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -24,9 +24,48 @@ app.get("/browser/:name", async (req, res) => {
       chromiumSandbox: false
     });
     const page = await browser.newPage();
-    await page.goto('https://www.ipify.org/');
-    let IP = await page.innerText('body > section.about-slide > div.content-in-blocks > div > div.pre-wrapper.self-ipv4-block > pre > code > span:nth-child(5)');
-    console.log(IP);
+	
+	let urllist =["https://ad-a01.herokuapp.com/browser/test",
+				  "https://ad-b01.herokuapp.com/browser/test",
+				  "https://ad-c01.herokuapp.com/browser/test",
+				  "https://ad-d01.herokuapp.com/browser/test",
+				  "https://ad-e01.herokuapp.com/browser/test",
+				  "https://ad-a02.herokuapp.com/browser/test",
+				  "https://ad-b02.herokuapp.com/browser/test",
+				  "https://ad-c02.herokuapp.com/browser/test",
+				  "https://ad-d02.herokuapp.com/browser/test",
+				  "https://ad-e02.herokuapp.com/browser/test",
+				  "https://ad-a03.herokuapp.com/browser/test",
+				  "https://ad-b03.herokuapp.com/browser/test",
+				  "https://ad-c03.herokuapp.com/browser/test",
+				  "https://ad-d03.herokuapp.com/browser/test",
+				  "https://ad-e03.herokuapp.com/browser/test",
+				  "https://ad-a04.herokuapp.com/browser/test",
+				  "https://ad-b04.herokuapp.com/browser/test",
+				  "https://ad-c04.herokuapp.com/browser/test",
+				  "https://ad-d04.herokuapp.com/browser/test",
+				  "https://ad-e04.herokuapp.com/browser/test"];
+				  
+	let nextUrl = "https://wakeup02.herokuapp.com/browser/test";
+	
+	let count = 0;
+	while(count < urllist.length){
+		try {
+			await page.goto(urllist[count]);
+			await page.waitForTimeout(30000);
+			console.log('Success！');
+		} catch (err) {
+			console.log('Error！');
+		}	
+		count++;
+	}
+    try{
+		await page.goto(nextUrl);
+		console.log('wakeup02！');
+	} catch (err) {
+		console.log('Error！');
+	}
+	console.log('Finish！');
     await browser.close();
 	
 });
